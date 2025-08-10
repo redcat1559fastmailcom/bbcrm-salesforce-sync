fcf78402638d4b75eedd0b572f7d281a596b61fb
Tim Holder <red.cat1559@fastmail.com>
2025-08-10 14:36:46 -0400

    - added load staging stored procedurw - TSQL script to create staging tables

---
+++ ERD SQL Database Project/StagingLoadTableProcedure.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE Load_All_Staging_Tables
+AS
+BEGIN
+    DECLARE @BatchID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @StartTime DATETIME = GETDATE();
+    DECLARE @TotalInserted INT = 0;
+    DECLARE @Error NVARCHAR(MAX) = NULL;
+
+    BEGIN TRY
+        -- Load Constituent
+        INSERT INTO Stg_Constituent (BBCRM_ID, FirstName, LastName, Email, DateChanged, LoadTimestamp, LoadBatchID)
+        SELECT BBCRM_ID, FirstName, LastName, Email, DateChanged, GETDATE(), @BatchID
+        FROM dbo.BBCRM_Constituent;
+        SET @TotalInserted += @@ROWCOUNT;
+
+        -- Load Address
+        INSERT INTO Stg_Address (BBCRM_ID, AddressLine1, City, State, Zip, DateChanged, LoadTimestamp, LoadBatchID)
+        SELECT BBCRM_ID, AddressLine1, City, State, Zip, DateChanged, GETDATE(), @BatchID
+        FROM dbo.BBCRM_Address;
+        SET @TotalInserted += @@ROWCOUNT;
+
+        -- Load Interaction
+        INSERT INTO Stg_Interaction (BBCRM_ID, InteractionDate, InteractionTypeID, Notes, DateChanged, LoadTimestamp, LoadBatchID)
+        SELECT BBCRM_ID, InteractionDate, InteractionTypeID, Notes, DateChanged, GETDATE(), @BatchID
+        FROM dbo.BBCRM_Interaction;
+        SET @TotalInserted += @@ROWCOUNT;
+
+        -- Load Address Type
+        INSERT INTO Stg_Address_Type (AddressTypeID, Description, DateChanged, LoadTimestamp, LoadBatchID)
+        SELECT AddressTypeID, Description, DateChanged, GETDATE(), @BatchID
+        FROM dbo.BBCRM_Address_Type;
+        SET @TotalInserted += @@ROWCOUNT;
+
+        -- Load Interaction Type
+        INSERT INTO Stg_Interaction_Type (InteractionTypeID, Description, DateChanged, LoadTimestamp, LoadBatchID)
+        SELECT InteractionTypeID, Description, DateChanged, GETDATE(), @BatchID
+        FROM dbo.BBCRM_Interaction_Type;
+        SET @TotalInserted += @@ROWCOUNT;
+
+        -- Log success
+        INSERT INTO StagingLoadLog (LoadBatchID, LoadStartTime, LoadEndTime, Source, Status, RecordsInserted)
+        VALUES (@BatchID, @StartTime, GETDATE(), 'BBCRM_All', 'Success', @TotalInserted);
+    END TRY
+
+    BEGIN CATCH
+        SET @Error = ERROR_MESSAGE();
+
+        -- Log failure
+        INSERT INTO StagingLoadLog (LoadBatchID, LoadStartTime, LoadEndTime, Source, Status, RecordsInserted, ErrorMessage)
+        VALUES (@BatchID, @StartTime, GETDATE(), 'BBCRM_All', 'Failed', @TotalInserted, @Error);
+    END CATCH
+END
---
+++ ERD SQL Database Project/TSQLStagingSchema.sql
@@ -0,0 +1,47 @@
+﻿CREATE TABLE Stg_BBCRM_Mock_Constituent (
+    BBCRM_ID UNIQUEIDENTIFIER PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Email NVARCHAR(100),
+    DateChanged DATETIME,
+    LoadTimestamp DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE Stg_BBCRM_Mock_Address (
+    Address_ID UNIQUEIDENTIFIER PRIMARY KEY,
+    Constituent_ID UNIQUEIDENTIFIER,
+    Street NVARCHAR(100),
+    City NVARCHAR(50),
+    [State] NVARCHAR(50),
+    ZIP NVARCHAR(20),
+    Address_Type_Code UNIQUEIDENTIFIER,
+    DateChanged DATETIME,
+    LoadTimestamp DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE Stg_BBCRM_Mock_Interaction (
+    Interaction_ID UNIQUEIDENTIFIER PRIMARY KEY,
+    Constituent_ID UNIQUEIDENTIFIER,
+    Interaction_Date DATETIME,
+    Notes NVARCHAR(MAX),
+    Interaction_Type_Code UNIQUEIDENTIFIER,
+    DateChanged DATETIME,
+    LoadTimestamp DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE Stg_BBCRM_Mock_Address_Type (
+    ID UNIQUEIDENTIFIER PRIMARY KEY,
+    Type_Description NVARCHAR(100),
+    DateChanged DATETIME,
+    LoadTimestamp DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE Stg_BBCRM_Mock_Interaction_Type (
+    ID UNIQUEIDENTIFIER PRIMARY KEY,
+    Type_Description NVARCHAR(100),
+    DateChanged DATETIME,
+    LoadTimestamp DATETIME DEFAULT GETDATE()
+);
+
+-- In StagingDB
+--CREATE SYNONYM BBCRM_Interaction_Type FOR BBCRM.dbo.BBCRM_Interaction_Type;
